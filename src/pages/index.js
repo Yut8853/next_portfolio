166d81f953f53c994532a9fa34eb74cae51b2ae1
yk0611 <hello@junkbranding.com>
2022-12-21 14:34:10 +0900

    next-blog-base

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import * as style from '../styles/index.module.css'
 
 const Index = () => {
   return (
-    <h1 className={style.h1Text}>Hello</h1>
+    <>
+      <h1 className={style.h1Text}>Hello</h1>
+      <Link href="/contact">Contactページへ移動</Link>
+    </>
   )
 }
 
